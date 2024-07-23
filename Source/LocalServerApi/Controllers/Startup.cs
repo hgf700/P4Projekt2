65ae35f50e629427cb66ca70afd49f4034da0b85
hgf700 <148117806+hgf700@users.noreply.github.com>
2024-07-23 21:43:46 +0200

    added 2 program

---
+++ Source/LocalServerApi/Controllers/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class Startup
+{
+    private readonly IConfiguration _configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Konfiguracja połączenia z PostgreSQL
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(_configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddControllersWithViews();
+    }
+
+    //public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    //{
+    //    // Inna konfiguracja aplikacji
+    //}
+}
---
+++ Source/LocalServerApi/Migrations/20240723095908_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace IdentityService.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "usersaveddata",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Granttype = table.Column<string>(type: "text", nullable: false),
+                    Firstname = table.Column<string>(type: "text", nullable: false),
+                    Lastname = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    ClientId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_usersaveddata", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "usersaveddata");
+        }
+    }
+}
