af638c5ef08eb2d9651f95a0658107a60cca1c4c
hgf700 <148117806+hgf700@users.noreply.github.com>
2024-07-24 13:12:09 +0200

    add 2 file

---
+++ Source/LocalServerApi/DataBase/ApplicationDbContext .cs
@@ -0,0 +1,29 @@
+﻿using IdentityService.DataBase;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    // Definiowanie DbSet dla Twoich encji
+    public DbSet<UserSavedData> usersaveddata{ get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<UserSavedData>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Granttype).IsRequired();
+            entity.Property(e => e.Firstname).IsRequired();
+            entity.Property(e => e.Lastname).IsRequired();
+            entity.Property(e => e.Email).IsRequired();
+            entity.Property(e => e.Password).IsRequired();
+            entity.Property(e => e.ClientId).IsRequired();
+        });
+    }
+}
---
+++ Source/LocalServerApi/DataBase/UserSavedData.cs
@@ -0,0 +1,13 @@
+﻿namespace IdentityService.DataBase
+{
+    public class UserSavedData
+    {
+        public int Id { get; set; }
+        public string Granttype { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ClientId { get; set; }
+    }
+}
